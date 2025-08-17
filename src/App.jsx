9d42871415f08ecab56a9127a3feeae4a5ac2038
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-08-17 08:44:50 +0545

    Add initial setup for Tailwind CSS, React components, and mouse follower
    - Created index.css for Tailwind CSS integration and custom styles
    - Implemented main.jsx to render the App component with MouseFollower
    - Configured tailwind.config.js for custom theme and container settings
    - Set up vite.config.js for Vite with React plugin

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
+
+
+
+function App() {
+
+  return (
+    <main className='overflow-x-hidden'>
+      <Hero/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import Logo from "../assets/logo.png";
+import { MdMenu } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { UpdateFollower } from "react-mouse-follower";
+
+const NavbarMenu = [
+  { id: 1, title: "Home", link: "#" },
+  { id: 2, title: "Categories", link: "#" },
+  { id: 3, title: "Blog", link: "#" },
+  { id: 4, title: "About", link: "#" },
+  { id: 5, title: "Contact", link: "#" },
+];
+
+const Navbar = () => {
+  return (
+    <div className="text-white py-3 md:py-8">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="container flex justify-between items-center"
+      >
+        {/* Logo */}
+        <div>
+          <img src={Logo} alt="Logo" className="max-w-[100px] invert" />
+        </div>
+
+        {/* Menu */}
+        <div className="hidden md:block">
+          <ul className="flex items-center gap-6 relative z-40">
+            {NavbarMenu.map((item) => (
+              <li key={item.id}>
+                <UpdateFollower
+                  mouseOptions={{
+                    backgroundColor: "white",
+                    followSpeed: 1.5,
+                    scale: 5,
+                    mixBlendMode: "difference",
+                  }}
+                >
+                  <a
+                    href={item.link}
+                    className="inline-block text-base font-semibold py-2 px-3 uppercase"
+                  >
+                    {item.title}
+                  </a>
+                </UpdateFollower>
+              </li>
+            ))}
+
+            {/* User Icon */}
+            <li>
+              <UpdateFollower
+                mouseOptions={{
+                  backgroundColor: "white",
+                  followSpeed: 1.5,
+                  scale: 5,
+                  mixBlendMode: "difference",
+                }}
+              >
+                <button className="text-xl ps-14">
+                  <FaRegUser />
+                </button>
+              </UpdateFollower>
+            </li>
+          </ul>
+        </div>
+
+        {/* Mobile Hamburger Icon */}
+        <div className="md:hidden">
+          <MdMenu className="text-4xl" />
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Navbar;
